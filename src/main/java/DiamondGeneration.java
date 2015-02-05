5ba09130f089f77d2d78c2530b4e27a408b73a34
DClaudio <claudio.david@LONWRK071.virtuefusion.corp>
2015-02-05 11:58:04 +0000

    junit setup

---
+++ src/main/java/DiamondGeneration.java
@@ -0,0 +1,9 @@
+/**
+ * Created by claudio.david on 05/02/2015.
+ */
+public class DiamondGeneration {
+    private char letter;
+    public DiamondGeneration(char letter) {
+        this.letter = letter;
+    }
+}
---
+++ src/test/java/DiamondGenerationTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by claudio.david on 05/02/2015.
+ */
+public class DiamondGenerationTest {
+
+    @Test
+    public void generateForATest(){
+        DiamondGeneration diaGen = new DiamondGeneration('A');
+    }
+}
