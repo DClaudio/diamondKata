d232a2114c6b8052319d3337bcc0e16d0cf5b311
DClaudio <claudio.david@LONWRK071.virtuefusion.corp>
2015-02-05 12:10:10 +0000

    added 2 unit tests

--- src/main/java/DiamondGeneration.java
+++ src/main/java/DiamondGeneration.java
@@ -2,8 +2,17 @@
  * Created by claudio.david on 05/02/2015.
  */
 public class DiamondGeneration {
-    private char letter;
+    private Character letter;
     public DiamondGeneration(char letter) {
         this.letter = letter;
+    }
+
+    public String generate() {
+        if(letter == 'B'){
+            return "  A \nB  B\n  A  ";
+        }else{
+            return "A";
+
+        }
     }
 }
--- src/test/java/DiamondGenerationTest.java
+++ src/test/java/DiamondGenerationTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by claudio.david on 05/02/2015.
  */
@@ -8,5 +10,14 @@ public class DiamondGenerationTest {
     @Test
     public void generateForATest(){
         DiamondGeneration diaGen = new DiamondGeneration('A');
+        String expectedResult = "A";
+        assertEquals("Assert for A letter diamond", expectedResult, diaGen.generate());
+    }
+
+    @Test
+    public void generateForBTest(){
+        DiamondGeneration diaGen = new DiamondGeneration('B');
+        String expectedResult = "  A \nB  B\n  A  ";
+        assertEquals("Assert for A letter diamond", expectedResult, diaGen.generate());
     }
 }
