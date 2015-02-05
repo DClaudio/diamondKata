99fb045c9e37d8485e12cc34cf85fe8f8080c43d
DClaudio <david_claudio_t@yahoo.com>
2015-02-05 14:12:29 +0000

    Did some refactoring

--- src/main/java/DiamondGeneration.java
+++ src/main/java/DiamondGeneration.java
@@ -2,19 +2,24 @@
  * Created by claudio.david on 05/02/2015.
  */
 public class DiamondGeneration {
+
     private Character letter;
+    private int characterCode;
+    private static int startingCharacterCode = String.valueOf('A').codePointAt(0);
+
     public DiamondGeneration(char letter) {
         this.letter = letter;
+        characterCode = String.valueOf(letter).codePointAt(0);
     }
 
     public String generate() {
-
-        int cp = String.valueOf(letter).codePointAt(0);
-        String result = "A";
-        if(letter == 'B'){
-            result = " A \n"+
-                     "B B\n"+
-                     " A ";
+        String result = "";
+        if (letter == 'B') {
+            result += " A \n";
+            result += "B B\n";
+            result += " A ";
+        } else{
+            result += "A";
         }
         return result;
     }
